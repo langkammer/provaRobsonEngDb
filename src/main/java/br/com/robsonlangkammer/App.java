9164dd143a3187d2499f03a918b135c0bb40809e
ROBSON UBUNTU CASAS <robsonlangkammer@hotmail.com>
2019-03-06 17:34:26 -0300

    finalizacao do proejto e configuração do jetty

--- src/main/java/br/com/robsonlangkammer/App.java
+++ src/main/java/br/com/robsonlangkammer/App.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+
 @SpringBootApplication
 public class App {
 
@@ -13,15 +14,5 @@ public class App {
     }
 
 
-
-//    @Bean
-//    public DataSource dataSource(){
-//        final DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//        dataSource.setDriverClassName(env.getProperty("driverClassName"));
-//        dataSource.setUrl(env.getProperty("url"));
-//        dataSource.setUsername(env.getProperty("user"));
-//        dataSource.setPassword(env.getProperty("password"));
-//        return dataSource;
-//    }
 
 }
